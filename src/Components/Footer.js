df73e66ab262b89d6d954cfd3494410d37c5c020
Ali Mert <mail@alimert.net>
2020-09-30 13:13:51 +0300

    xxx

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import Divider from '@material-ui/core/Divider';
 import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
+
 import Grid from "@material-ui/core/grid";
 import Iyzico from "../Svg/iyzico.svg";
 import Facebook from "../Svg/Social/facebook.svg";
 import Insta from "../Svg/Social/instagram.svg";
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import Kargo from "../Img/kargo.png";
+import { ReactComponent as GooglePlay } from "../Svg/google-play.svg";
+
+import IconButton from '@material-ui/core/IconButton';
 
 const useStyles = makeStyles((theme) =>
     ({
+        upperContainer: { marginBottom: 20 },
         social: {
             margin: 5,
         },
@@ -24,13 +31,41 @@ export default function Footer() {
     return (
         <>
             <Container>
-                x
-                <Grid container>
-                    <Grid item xs={7}>
-                        sol
+                <Grid container className={classes.upperContainer}>
+                    <Grid container item xs={7}>
+                        <Grid container item xs={5}>
+                            <Grid item xs={12}>FormaBorsa</Grid>
+                            <Grid item xs={12}>Neden FormaBorsa?</Grid>
+                            <Grid item xs={12}>Üyelik Sözleşmesi</Grid>
+                            <Grid item xs={12}>Kullanım Koşulları</Grid>
+                            <Grid item xs={12}>Kişisel Veri Politikası</Grid>
+                            <Grid item xs={12}>FormaBorsa API</Grid>
+                            <Grid item xs={12}>Gizlilik Politikası</Grid>
+                            <Grid item xs={12}>İletişim</Grid>
+                            <Grid item xs={12}>Güvenli Ödeme Sistemi</Grid>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <Grid item xs={12}>Yardım</Grid>
+                            <Grid item xs={12}>Sıkça Sorulan Sorular</Grid>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <Grid item xs={12}>Whatsapp & İletişim</Grid>
+                            <Grid item xs={12}>+90 546 568 0707</Grid>
+                            <Grid item xs={12}>+90 542 622 2077</Grid>
+                            <Grid item xs={12}>+850 533 0633</Grid>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={5}>
-                        sağ
+                    <Grid container item xs={5}>
+                        <Grid item xs={12}>
+                            <img src={GooglePlay} aria-hidden="true" alt="" height="24px"></img>App Store
+                            <Button variant="contained"
+                                color="primary"
+                                size="large" startIcon={<GooglePlay />} >Google Play indir</Button>
+
+                        </Grid>
+                        <Grid item xs={12}>
+                            <img src={Kargo} aria-hidden="true" alt="" height="48px"></img>
+                        </Grid>
                     </Grid>
                 </Grid>
             </Container>
@@ -38,7 +73,7 @@ export default function Footer() {
             <Container>
                 <Grid container>
                     <Grid item xs={4} style={{ marginTop: 5 }}>
-                        <Typography>
+                        <Typography component="p" variant="caption">
                             © 2018-2020 farmaborsa.com® | Tüm hakları saklıdır
                         </Typography>
                     </Grid>
